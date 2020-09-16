62349d3df854e98f6dbc3bac1803d8d507ad1186
Wesley Ramires <weesleyramires@gmail.com>
2020-09-16 13:51:26 -0300

    Criando navegação com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
+import { Route } from 'react-router-dom';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <Routes />
   );
 }
 
