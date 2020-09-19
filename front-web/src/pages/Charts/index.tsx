64b18fa711d4d28e485191c88550b2bf29353e44
Wesley Ramires <weesleyramires@gmail.com>
2020-09-18 23:54:53 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-wesley.herokuapp.com';
 
 const Charts = () =>  {
     const [ barChartData, setBarChatData] = useState<BarChatData[]>([]);
